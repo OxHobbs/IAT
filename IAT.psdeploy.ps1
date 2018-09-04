d0abd12e60d6d95eea50fbce00464b87cfa980f5
Austin Hobbs <austin.hobbs@microsoft.com>
2018-09-04 17:06:22 -0500

    build update

--- IAT.psdeploy.ps1
+++ IAT.psdeploy.ps1
@@ -1,8 +1,20 @@
 Deploy "Deploy to Dev" {
+    By Task PackageInVersionDir {
+        $manifest = Import-LocalizedData -FileName 'IAT.psd1' -BaseDirectory 'IAT'
+        $version = $manifest.ModuleVersion
+        $deployDir = "c:\deployments\dev\IAT\$version"
+
+        if (Test-Path $deployDir)
+        {
+            throw "Version is already deployed.  Must bump version properly before deploying a new version"
+        }
+    }
+
     By FileSystem  {
         FromSource 'IAT'
-        To 'c:\deployments\dev'
+        To $deployDir
         Tagged Dev
+        DependingOn 'PackageVersionDir'
         WithOptions @{
             Mirror = $true
         }
@@ -10,10 +22,22 @@ Deploy "Deploy to Dev" {
 }
 
 Deploy "Deploy to Prod" {
+    By Task PackageInVersionDir {
+        $manifest = Import-LocalizedData -FileName 'IAT.psd1' -BaseDirectory 'IAT'
+        $version = $manifest.ModuleVersion
+        $deployDir = "c:\deployments\prod\IAT\$version"
+
+        if (Test-Path $deployDir)
+        {
+            throw "Version is already deployed.  Must bump version properly before deploying a new version"
+        }
+    }
+
     By FileSystem  {
         FromSource 'IAT'
-        To 'c:\deployments\prod'
+        To $deployDir
         Tagged Prod
+        DependingOn 'PackageVersionDir'
         WithOptions @{
             Mirror = $true
         }
