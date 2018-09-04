571f8846690a0966ab80e8026c1ad83d7c6b2030
Austin Hobbs <austin.hobbs@microsoft.com>
2018-09-04 12:21:57 -0500

    updated build script to force and not confirm

--- build.ps1
+++ build.ps1
@@ -9,9 +9,9 @@ param
 $ErrorActionPreference = 'Stop'
 $PSScriptRoot = Split-Path $MyInvocation.MyCommand.Path -Parent
 
-if (-not (Get-Module -Name Pester -ListAvailable))   { Install-Module -Name Pester -Scope CurrentUser }
-if (-not (Get-Module -Name psake -ListAvailable))    { Install-Module -Name psake -Scope CurrentUser }
-if (-not (Get-Module -Name PSDeploy -ListAvailable)) { Install-Module -Name PSDeploy -Scope CurrentUser }
+if (-not (Get-Module -Name Pester -ListAvailable))   { Install-Module -Name Pester -Force -Confirm:$false }
+if (-not (Get-Module -Name psake -ListAvailable))    { Install-Module -Name psake -Force -Confirm:$false }
+if (-not (Get-Module -Name PSDeploy -ListAvailable)) { Install-Module -Name PSDeploy -Force -Confirm:$false }
 
 Invoke-psake -buildFile "$PSScriptRoot\psakeBuild.ps1" -taskList $Task -Verbose:$VerbosePreference
 
