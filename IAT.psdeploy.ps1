475e57703b40d051a2e7515bc24eb498e0626314
Austin Hobbs <austin.hobbs@microsoft.com>
2018-09-04 17:24:38 -0500

    changed scope

--- IAT.psdeploy.ps1
+++ IAT.psdeploy.ps1
@@ -2,17 +2,19 @@ Deploy "Deploy to Dev" {
     By Task PackageInVersionDir {
         $manifest = Import-LocalizedData -FileName 'IAT.psd1' -BaseDirectory 'IAT'
         $version = $manifest.ModuleVersion
-        $deployDir = "c:\deployments\dev\IAT\$version"
+        $script:deployDir = "c:\deployments\dev\IAT\$version"
 
         if (Test-Path $deployDir)
         {
             throw "Version is already deployed.  Must bump version properly before deploying a new version"
         }
+
+        Copy-Item -Path IAT -Recurse -Destination $deployDir
     }
 
     By FileSystem  {
         FromSource 'IAT'
-        To $deployDir
+        To $script:deployDir
         Tagged Dev
         DependingOn 'PackageVersionDir'
         WithOptions @{
@@ -25,7 +27,7 @@ Deploy "Deploy to Prod" {
     By Task PackageInVersionDir {
         $manifest = Import-LocalizedData -FileName 'IAT.psd1' -BaseDirectory 'IAT'
         $version = $manifest.ModuleVersion
-        $deployDir = "c:\deployments\prod\IAT\$version"
+        $script:deployDir = "c:\deployments\prod\IAT\$version"
 
         if (Test-Path $deployDir)
         {
@@ -35,7 +37,7 @@ Deploy "Deploy to Prod" {
 
     By FileSystem  {
         FromSource 'IAT'
-        To $deployDir
+        To $script:deployDir
         Tagged Prod
         DependingOn 'PackageVersionDir'
         WithOptions @{
